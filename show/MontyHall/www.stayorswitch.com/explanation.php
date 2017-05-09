217ea1aff3018db8ea30dfbe708c562ee489aa9d
Yixf(Yi Xianfu) <yixfgit@gmail.com>
2017-05-09 09:26:01 +0800

    Add MontyHall

---
+++ show/MontyHall/www.stayorswitch.com/explanation.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+
+<meta property="fb:admins" content="105400023">
+<meta name="description" content="Why is it better to switch doors? Multiple explanations of the Monty Hall Problem." />
+<meta name="keywords" content="monty hall,monty hall problem, monte hall, probability,brain teaser,marilyn vos savant, game show, stay or switch" />
+<link rel="shortcut icon" type="image/x-icon" href="http://www.stayorswitch.com/Images/favicon.ico">
+<title>Monty Hall Problem Explained With Worked Solutions</title>
+<style type="text/css">
+<!--
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	background-color:#7c7c7c;
+}
+#apDiv1 {
+	position:absolute;
+	width:200px;
+	height:115px;
+	z-index:1;
+	left: 551px;
+	top: 233px;
+}
+.no_tile {
+	background-image: url(Images/background_3.png);
+	background-repeat: no-repeat;
+	background-position: left top;
+}
+.stats {
+	font-size: 24px;
+	color: #f4f4f4;
+	font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+	width: auto;
+	letter-spacing: -1.5pt;
+	text-align: justify;
+	line-height: 18pt;
+}
+.nonitalic {
+	font-style: normal;
+}
+.stats_nums {
+	font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+	font-size: 30px;
+	line-height: 18pt;
+}
+.stats_small {
+	font-size: 18px;
+	font-weight: bolder;
+	line-height: 20pt;
+}
+#footernav {
+	clear: both;
+	margin-left:220px;
+	padding-top: 20px;
+	color: #f4f4f4;
+	font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+}
+a {
+    color: #f4f4f4;
+    text-decoration: none;
+}
+
+a:hover 
+{
+     color:#f4f4f4; 
+     text-decoration:none;
+}
+#clicker
+{
+	width: 430px;
+	height: 90px;
+	display: block;
+	position: relative;
+	margin-bottom: -90px;
+}
+<!-- Begin MailChimp Form Style-->
+#mc_embed_signup form {display:block; position:relative; text-align:left; padding:10px 0 10px 3%}
+#mc_embed_signup h2 {font-weight:bold; padding:0; margin:15px 0; font-size:1.4em;}
+#mc_embed_signup input {border:1px solid #999; -webkit-appearance:none;}
+#mc_embed_signup input[type=checkbox]{-webkit-appearance:checkbox;}
+#mc_embed_signup input[type=radio]{-webkit-appearance:radio;}
+#mc_embed_signup input:focus {border-color:#333;}
+#mc_embed_signup .button {clear:both; background-color: #00CC33; border: 0 none; border-radius:4px; color: #FFFFFF; cursor: pointer; display: inline-block; font-size:15px; font-weight: bold; height: 32px; line-height: 32px; margin: 0 5px 10px 0; padding:0; text-align: center; text-decoration: none; vertical-align: top; white-space: nowrap; width: auto;}
+#mc_embed_signup .button:hover {background-color:#777;}
+#mc_embed_signup .small-meta {font-size: 11px;}
+#mc_embed_signup .nowrap {white-space:nowrap;}     
+#mc_embed_signup .clear {clear:none; display:inline;}
+
+#mc_embed_signup label {display:block; font-size:18px; padding-bottom:10px; color: #f4f4f4; font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;}
+#mc_embed_signup input.email {display:block; padding:8px 0; margin:0 4% 10px 0; text-indent:5px; width:75%; min-width:130px;}
+#mc_embed_signup input.button {display:block; width:100%; margin:0 0 0 0; min-width:90px;}
+
+#mc_embed_signup div#mce-responses {float:left; top:-1.4em; padding:0em .5em 0em .5em; overflow:hidden; width:90%;margin: 0 5%; clear: both;}
+#mc_embed_signup div.response {margin:1em 0; padding:1em .5em .5em 0; font-weight:bold; float:left; top:-1.5em; z-index:1; width:80%;}
+#mc_embed_signup #mce-error-response {display:none;}
+#mc_embed_signup #mce-success-response {color:#529214; display:none;}
+#mc_embed_signup label.error {display:block; float:none; width:auto; margin-left:1.05em; text-align:left; padding:.5em 0;}
+<!-- End MailChimp Form Style-->
+-->
+</style>
+<script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
+<script type="text/javascript">
+<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-24711987-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+
+<body onload="MM_preloadImages('Images/navbar_playgame_roll.png','Images/navbar_explanation_roll.png','Images/navbar_blog_roll.png')">
+
+
+<table width="100%" border="0" cellpadding="0">
+  <tr>
+    <td align="center" valign="top"><table width="866" border="0" cellpadding="0" class="no_tile">
+      <tr>
+        <td colspan="4" align="left" valign="bottom">
+        <a id="clicker" href="index.html"></a>
+        <div align="left" style="float:left; padding-top:70px; padding-left:43px; font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif; color:#f4f4f4; font-size:13px; line-height:27px">
+            <h1><a href="index.html">The Monty <br />
+              Hall Problem</a></h1>
+</div><div style="padding-top:68px; padding-bottom:30px"><a href="index.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('playGame','','Images/navbar_playgame_roll.png',1)"><img src="Images/navbar_playgame.png" alt="PLAY GAME" name="playGame" width="206" height="76" border="0" id="playGame" /></a><a href="explanation.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('explanation','','Images/navbar_explanation_bold.png',1)"><img src="Images/navbar_explanation_bold.png" alt="EXPLANATION" name="explanation" width="206" height="76" border="0" id="explanation" /></a><a href="blog" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('blog','','Images/navbar_blog_roll.png',1)"><img src="Images/navbar_blog.png" alt="BLOG" name="blog" width="206" height="76" border="0" id="blog" /></a></div>
+		</td>
+      </tr>
+     <tr><td colspan="2"><script async src="http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Links above Player -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:15px"
+     data-ad-client="ca-pub-1582010447511354"
+     data-ad-slot="4786850668"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></td>
+          </tr>
+        <tr><td width="594" height="430" align="right" valign="top" style="padding-top: 16px; padding-bottom: 10px; padding-left:30px"><iframe width="540" height="400" src="http://www.youtube.com/embed/BigmlbUOxrc?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe></td>
+        <td colspan="2" align="center" valign="top" class="stats" style="padding-top:22px; padding-left:28px"><div style="margin-top: 20px; border-bottom: 2px solid rgb(192, 192, 192); padding-bottom: 24px; width: 189px;">
+players who <strong><em><font color="#386be5" class="nonitalic" style="normal">STAY</font></em></strong> have won <span class="stats"><strong>160070</strong></span> cars out of <strong class="stats">476458</strong> games yielding a winning <span class="lastline">percentage of </span><span class="stats_nums"><strong class="lastline">34</strong></span><span class="lastline"><strong>%</strong></span>
+
+</div>          <span class="stats" style="padding-top:15px">
+        
+  <div style="height: 170px; margin-top: 20px; width: 189px;">
+players who <strong><em><font color="#386be5" class="nonitalic">SWITCH</font></em></strong> have won <strong class="stats">180629</strong> cars out of <strong class="stats">272455</strong> games yielding a winning percentage of <strong class="stats_nums">66</strong><strong>%</strong>
+</div></span></td>
+<td valign="top">
+         <div id="mc_embed_signup"  style="margin-top:-13px; margin-left:40px; width:200px; background:#386be5; padding:10px;">
+	<form action="http://stayorswitch.us7.list-manage.com/subscribe/post?u=1e86cc96cf53914baeed88866&amp;id=bdd65fa1e6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+	<label for="mce-EMAIL">My Top 13 Simple Math Puzzles Most People Can't Solve<br><font size=2pt>(Hint: They All Have Elegant Answers)</font></label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+	<div class="clear"><input type="submit" value="Download the Free PDF" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+	</form>
+          </td>
+      </tr>
+      <tr>
+        <td colspan="3" align="left" valign="top"><div style="padding-top: 0px; padding-left: 36px; padding-right: 34px; float: left; padding-bottom: 22px; color:#F4F4F4; font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif;">
+          <strong>Video Transcript</strong>:
+            <br />
+You’re on a game show and there are three doors in front of you. The host, Monty Hall, says, “Behind one door is a brand new car. Behind the other two doors are goats. Pick a door!”
+            
+            You think, “Well, it doesn’t matter which door I choose, every door has a 1/3 chance of having the car behind it.” So, you choose door number 1.
+            
+            Now it gets interesting. Monty, the host, who knows where the car is, opens door number 2 and reveals a goat. The host always opens a door to reveal a goat. The host says, “If you want, you can switch to door number 3.” What should you do? Stay with your original choice or switch to the other door?
+            
+            All right, so what are you going to do? Stay or switch? Well, it’s a fifty-fifty chance of winning the car in either door. Right? [Wrong!] You actually double your chances of winning the car by switching doors. And that is why the Monty Hall Problem is so evasive!
+<p>Choose an explanation to the Monty Hall Problem:            </p><h2 style="font-size:18px; margin-bottom:-1px">1/3 vs 2/3 – Solution #1 to the Monty Hall Problem</h2>
+There is a 1/3 chance of the car being behind door number 1 and a 2/3 chance that the car isn’t behind door number 1. After Monty Hall opens door number 2 to reveal a goat, there’s still a 1/3 chance that the car is behind door number 1 and a 2/3 chance that the car isn’t behind door number 1. A 2/3 chance that the car isn’t behind door number 1 is a 2/3 chance that the car is behind door number 3.<h2 style="font-size:18px; margin-bottom:-1px">100 Doors! – Solution #2 to the Monty Hall Problem
+            </h2>
+            Imagine that instead of 3 doors, there are 100. All of them have goats except one, which has the car. You choose a door, say, door number 23. At this point, Monty Hall opens all of the other doors except one and gives you the offer to switch to the other door. Would you switch? Now you may arrogantly think, “Well, maybe I actually picked the correct door on my first guess.” But what’s the probability that that happened? 1/100. There’s a 99% chance that the car isn’t behind the door that you picked. And if it’s not behind the door that you picked, it must be behind the last door that Monty left for you. In other words, Monty has helped you by leaving one door for you to switch to, that has a 99% chance of having the car behind it.  So in this case, if you were to switch, you would have a 99% chance of winning the car.<h2 style="font-size:18px; margin-bottom:-1px">Pick a Goat – Solution #3 to the Monty Hall Problem</h2>
+To win using the stay strategy, you need to choose the car on your first pick because you’re planning to stay with your initial choice. The chance of picking the car on your first pick is clearly one out of three. But, in order to win using the switch strategy, you only need to pick a goat on your first pick because the host will reveal the other goat and you’ll end up switching to the car. So you want to use the strategy that lets you win if you choose a goat initially because you’re twice as likely to start by picking a goat.<h2 style="font-size:18px; margin-bottom:-1px">Scenarios – Solution #4 to the Monty Hall Problem
+            </h2>
+            To understand why it’s better to switch doors, let’s play out a few scenarios. Let’s see what will happen if you were to always stay with your original choice. We’ll play out three scenarios, one for each door that the car could be behind (door number 1, door number 2, or door number 3). And it doesn’t matter which door you start out with, so, to keep it simple, we’ll always start by choosing door number 1.              </p>
+            <p>Stay strategy, scenario 1: the car is behind door number 1. You choose door number 1, then  the host reveals a goat behind door number 2 and because you always stay, you stay with door number 1. You win the car! Stay strategy, scenario 2: the car is behind door number 2.  You start by picking door number 1, the host reveals a goat behind door number 3, and you’re using the stay strategy so you stay with door number 1. You get a goat and don’t win the car. Stay strategy, scenario 3: the car is behind door number 3. You pick door number 1, the host opens door number 2 to reveal a goat, you stay with door number 1, and you get a goat. So, using the stay strategy, you won the car one out of three times. That means that in any one instance of playing the game, your chance of winning the car if you choose to stay is 1/3 or about 33%.              </p>
+            <p>Now let’s try switching doors. Again, we’ll always start by picking door number 1. Switch strategy, scenario 1: the car is behind door number 1. You choose door number 1, the host opens door number 2 to reveal a goat, you are using the switch strategy so you switch to door number 3. You get a goat. Switch strategy, scenario 2: the car is behind door number 2. You start by picking door number 1, the host opens door number 3 to reveal a goat, you switch to door number 2 and win the car! Switch strategy, scenario 3: the car is behind door number 3. You pick door number 1, the host opens door number 2 to reveal a goat, you switch to door number 3 and win the car again! So, with the switch strategy you won the car 2 out of 3 times. That means, that in any one instance of the game, your chance of winning the car if you choose to switch doors is 2/3 or about 67%.              </p>
+          <p>Therefore, if you play the game three times and stay, on average you’ll win the car once. But if you play the game three times and switch each time, on average you’ll win the car twice. That’s twice as many cars!</p></div></td>
+        </tr>
+      <tr><td>
+         <!-- Footer Navigation -->
+<div id="footernav">
+<hr style="
+    margin-left: -200px;
+    margin-right: -250px;
+">
+<a href="index.html">Play&nbsp;</a>
+|
+<a href="explanation.php">&nbsp;Explanation&nbsp;</a>
+|
+<a href="blog">&nbsp;Blog&nbsp;</a>
+|
+<a href="http://www.stayorswitch.com/history.html">&nbsp;History&nbsp;</a>
+|
+<a href="http://www.stayorswitch.com/about.html">&nbsp;About</a>
+</div>
+<!-- End Footer Navigation -->
+      <p><span style="color:#f4f4f4; font-family:'Trebuchet MS', Arial, Helvetica, sans-serif; font-size:12px; margin-left:280px;">&copy; 2014 <a href="http://www.stayorswitch.com/about.html" rel="author">Jeremy Jones</a>. All rights reserved.</span></p></tr>
+     
+  </td></tr>
+</table></table>
+<script type="text/javascript">
+<!--
+swfobject.registerObject("FlashID");
+//-->
+</script>
+</body>
+</html>
